cc4ef6fbad21507ea5f0bb6632e538d2f3aecb46
Gabriela Guedes <zooiv3rde@gmail.com>
2019-03-31 20:59:09 -0300

    especificando cardinalidades

--- src/br/com/alura/financas/modelo/Movimentacao.java
+++ src/br/com/alura/financas/modelo/Movimentacao.java
@@ -2,6 +2,7 @@ package br.com.alura.financas.modelo;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,7 +10,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -32,6 +35,9 @@ public class Movimentacao {
 	
 	@ManyToOne
 	private Conta conta;
+	
+	@ManyToMany
+	private List<Categoria> categoria;
 
 	public Integer getId() {
 		return id;
