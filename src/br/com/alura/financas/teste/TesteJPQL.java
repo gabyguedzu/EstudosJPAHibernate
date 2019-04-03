4e6a0867f713b937ad2c4a0de1a68891ba54250a
Gabriela Guedes <zooiv3rde@gmail.com>
2019-04-02 23:05:47 -0300

    jpql

---
+++ src/br/com/alura/financas/teste/TesteJPQL.java
@@ -0,0 +1,33 @@
+package br.com.alura.financas.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.alura.financas.modelo.Movimentacao;
+import br.com.alura.financas.util.JPAUtil;
+
+public class TesteJPQL {
+	
+	public static void main(String[] args) {
+		EntityManager em = new JPAUtil().getEntityManager();
+		
+		em.getTransaction().begin();
+		
+		String jpql = "select m from Movimentacao m where m.conta.id = 2";
+		Query query = em.createQuery(jpql);
+		
+		List<Movimentacao> resultados = query.getResultList();
+		
+		for (Movimentacao movimentacao : resultados) {
+			System.out.println("Descrição: " + movimentacao);
+			System.out.println("Conta id: " + movimentacao.getConta().getId());
+		}
+		
+		em.getTransaction().commit();
+		
+		em.close();
+	}
+
+}
