e8e30551b3542111b2b72397194027ed5a809135
Gabriela Guedes <zooiv3rde@gmail.com>
2019-04-06 22:01:45 -0300

    finish

--- src/br/com/alura/financas/teste/TesteFuncoesJPQL.java
+++ src/br/com/alura/financas/teste/TesteFuncoesJPQL.java
@@ -1,32 +1,35 @@
 package br.com.alura.financas.teste;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import br.com.alura.financas.modelo.Conta;
 import br.com.alura.financas.modelo.Movimentacao;
+import br.com.alura.financas.modelo.TipoMovimentacao;
 import br.com.alura.financas.util.JPAUtil;
 
 public class TesteFuncoesJPQL {
 	
 	public static void main(String[] args) {
-		EntityManager em = new JPAUtil().getEntityManager();
 		
+		EntityManager em = new JPAUtil().getEntityManager();		
 		em.getTransaction().begin();
 		
+		Conta conta = new Conta();
 		String jpql = "select m from Movimentacao m where m.conta.id = 2";
 		Query query = em.createQuery(jpql);
+		query.setParameter("pConta", conta);
+		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
+		
+		BigDecimal soma = (BigDecimal) query.getSingleResult();
+		System.out.println("A soma é: " + soma);
 		
 		List<Movimentacao> resultados = query.getResultList();
 		
-		for (Movimentacao movimentacao : resultados) {
-			System.out.println("Descrição: " + movimentacao);
-			System.out.println("Conta id: " + movimentacao.getConta().getId());
-		}
-		
-		em.getTransaction().commit();
-		
+		em.getTransaction().commit();		
 		em.close();
 	}
 
